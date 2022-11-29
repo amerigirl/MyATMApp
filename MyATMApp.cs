0423f8aeb37041c7ff6a7b8de55e015f2108f05a
Be-Asia <93811176+amerigirl@users.noreply.github.com>
2022-11-28 22:17:09 -0600

    Add project files.

---
+++ MyATMApp.cs
@@ -0,0 +1,20 @@
+﻿namespace MyATMApp
+{
+    internal class MyATMApp
+    {
+        static void Main(string[] args)
+        {
+
+            //enjoying the console changes
+            Console.Title = "My ATM App"; 
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            
+            //prompting and welcoming the user
+            Console.WriteLine("\n\nGreetings and welcome to The Wacky Warehouse: " +
+                              "\nThe only place for all of your financial needs! " +
+                              "\n\nPlease press Enter to continue...\n");
+            Console.ReadLine();
+
+        }
+    }
+}
